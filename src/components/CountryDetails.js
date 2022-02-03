76e51fa815e959e8242651e8d5b4eada2cd7402e
hassan sabeh <h.adamsabeh@yahoo.com>
2022-02-03 21:21:26 +0100

    WIP

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -3,14 +3,15 @@ import countriesData from '../countries.json';
 
 
 const CountryDetails = (props) => {
-    console.log("details mounted")
     const countryId = props.match.params.countryId;
-    const country = countriesData.map(countryObj => {
-        if (countryObj.cca2 === countryId) return countryObj
+    const country = countriesData.filter(countryObj => {
+        if (countryObj.cca2 === countryId) {
+            return countryObj;
+        }
     })
     return (
         <>
-            {country.name}
+            <h1>{country[0].name.official}</h1>
         </>
     ); 
 }
